915264c41ac77fecd4a2ac356c180316f28a5413
Mikel <35065103+MikelEulate@users.noreply.github.com>
2018-11-20 12:09:12 +0100

    Update account_balance.py

--- account_balance.py
+++ account_balance.py
@@ -10,9 +10,76 @@ client = Client(api_key, api_secret)
 
 # Get account info
 info = client.get_account()
+# Example response:
+"""
+{
+    "makerCommission": 15,
+    "takerCommission": 15,
+    "buyerCommission": 0,
+    "sellerCommission": 0,
+    "canTrade": true,
+    "canWithdraw": true,
+    "canDeposit": true,
+    "balances": [
+        {
+            "asset": "BTC",
+            "free": "4723846.89208129",
+            "locked": "0.00000000"
+        },
+        {
+            "asset": "LTC",
+            "free": "4763368.68006011",
+            "locked": "0.00000000"
+        }
+    ]
+}
+"""
 
 # Get asset balance
 balance = client.get_asset_balance(asset='BTC')
+# Example response:
+"""
+{
+    "asset": "BTC",
+    "free": "4723846.89208129",
+    "locked": "0.00000000"
+}
+"""
 
 # Get account status
 status = client.get_account_status()
+# Example response:
+"""
+{
+    "msg": "Order failed:Low Order fill rate! Will be reactivated after 5 minutes.",
+    "success": true,
+    "objs": [
+        "5"
+    ]
+}
+"""
+
+# Getting the open orders of a symbol (after getting the account info and the balance of the traded cryptos)
+orders = client.get_open_orders(symbol='BNBBTC')
+## or of all the open orders:
+orders = client.get_open_orders()
+# Example response:
+"""
+[
+    {
+        "symbol": "LTCBTC",
+        "orderId": 1,
+        "clientOrderId": "myOrder1",
+        "price": "0.1",
+        "origQty": "1.0",
+        "executedQty": "0.0",
+        "status": "NEW",
+        "timeInForce": "GTC",
+        "type": "LIMIT",
+        "side": "BUY",
+        "stopPrice": "0.0",
+        "icebergQty": "0.0",
+        "time": 1499827319559
+    }
+]
+"""
